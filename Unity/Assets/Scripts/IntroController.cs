449704e3902cc91bb9208fda3fa6c81537c65082
NamSeungil <namseungngil@gmail.com>
2016-08-10 20:38:06 +0900

    third study commit

--- Unity/Assets/Scripts/IntroController.cs
+++ Unity/Assets/Scripts/IntroController.cs
@@ -4,4 +4,12 @@ using Hellgate;
 
 public class IntroController : SceneController
 {
+    public override void OnSet (object data)
+    {
+        base.OnSet (data);
+
+        SceneManager.Instance.Wait (3f, delegate() {
+            SceneManager.Instance.Screen ("Login");
+        });
+    }
 }
---
+++ Unity/Assets/Scripts/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hellgate;
+
+public class LoginController : SceneController
+{
+    private AssetBundleInitialDownloader downloader;
+
+    public override void OnSet (object data)
+    {
+        base.OnSet (data);
+
+        downloader = new AssetBundleInitialDownloader (
+            "https://dl.dropboxusercontent.com/u/95277951/gameon/resource.json",
+            "https://dl.dropboxusercontent.com/u/95277951/gameon/" + Util.GetDevice ());
+
+        downloader.aEvent = Callback;
+        downloader.Download ();
+    }
+
+    private void Callback (AssetBundleInitalStatus status)
+    {
+        switch (status) {
+        case AssetBundleInitalStatus.Start:
+            Debug.Log ("Start");
+        break;
+//        case AssetBundleInitalStatus.HttpError:
+        case AssetBundleInitalStatus.HttpTimeover:
+            Debug.Log ("Error");
+        break;
+        case AssetBundleInitalStatus.DownloadOver:
+        case AssetBundleInitalStatus.Over:
+            Debug.Log ("Over");
+        break;
+        default:
+        break;
+        }
+    }
+}
