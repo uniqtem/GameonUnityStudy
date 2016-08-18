f53a85800aa90aa4621c75d873726d2b08267dc1
NamSeungil <namseungngil@gmail.com>
2016-08-18 20:29:59 +0900

    4th commit

--- Unity/Assets/Scripts/LoginController.cs
+++ Unity/Assets/Scripts/LoginController.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using Hellgate;
 
 public class LoginController : SceneController
 {
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private Scrollbar scrollbar;
+    [SerializeField]
+    private Button button;
+
     private AssetBundleInitialDownloader downloader;
+    private AssetBundleInitalStatus mStatus;
 
     public override void OnSet (object data)
     {
         base.OnSet (data);
+
+        text.gameObject.SetActive (false);
+        scrollbar.gameObject.SetActive (false);
+        button.gameObject.SetActive (false);
 
         downloader = new AssetBundleInitialDownloader (
             "https://dl.dropboxusercontent.com/u/95277951/gameon/resource.json",
@@ -16,21 +29,58 @@ public class LoginController : SceneController
 
         downloader.aEvent = Callback;
         downloader.Download ();
+
+        button.onClick.AddListener (delegate() {
+            Debug.Log ("OnClick");
+
+            AssetBundleData assetBundle = new AssetBundleData ("master");
+            assetBundle.type = typeof(TextAsset);
+            assetBundle.objName = "quest";
+
+            LoadingJobData jobData = new LoadingJobData ("Quest");
+            jobData.assetBundles.Add (assetBundle);
+
+            SceneManager.Instance.LoadingJob (jobData);
+        });
+    }
+
+    void Update ()
+    {
+        if (mStatus == AssetBundleInitalStatus.Start) {
+            text.text = downloader.CurretIndex + " / " +
+            downloader.DownloadCount;
+            scrollbar.size = downloader.Progress;
+        }
     }
 
     private void Callback (AssetBundleInitalStatus status)
     {
+        mStatus = status;
+
         switch (status) {
         case AssetBundleInitalStatus.Start:
             Debug.Log ("Start");
+
+            text.gameObject.SetActive (true);
+            scrollbar.gameObject.SetActive (true);
         break;
-//        case AssetBundleInitalStatus.HttpError:
+        case AssetBundleInitalStatus.HttpError:
         case AssetBundleInitalStatus.HttpTimeover:
             Debug.Log ("Error");
+
+            SceneManager.Instance.PopUp ("Error " + status,
+                                         PopUpType.Ok,
+                                         delegate(PopUpYNType type) {
+                SceneManager.Instance.Screen ("Intro");
+            });
         break;
         case AssetBundleInitalStatus.DownloadOver:
         case AssetBundleInitalStatus.Over:
             Debug.Log ("Over");
+
+            text.gameObject.SetActive (false);
+            scrollbar.gameObject.SetActive (false);
+            button.gameObject.SetActive (true);
         break;
         default:
         break;
---
+++ Unity/Assets/Scripts/QuestController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+using Hellgate;
+
+public class QuestData
+{
+    private int idx = 0;
+    private string name = "";
+    private int monster_count;
+    private int monster_speed;
+
+    public int Idx {
+        get {
+            return idx;
+        }
+    }
+
+    public string Name {
+        get {
+            return name;
+        }
+    }
+}
+
+public class QuestController : SceneController
+{
+    [SerializeField]
+    private GameObject temp;
+    [SerializeField]
+    private GameObject content;
+
+    public override void OnSet (object data)
+    {
+        base.OnSet (data);
+
+        List<object> listObj = data as List<object>;
+
+        TextAsset textAsset = listObj.GetListObject<TextAsset> ();
+
+        IList iList = (IList)Json.Deserialize (textAsset.text);
+        QuestData[] questDatas = Reflection.Convert<QuestData> (iList);
+
+        for (int i = 0; i < questDatas.Length; i++) {
+            GameObject gObj = Instantiate (temp) as GameObject;
+            gObj.transform.SetParent (content.transform);
+            gObj.transform.localScale = Vector2.one;
+            gObj.transform.localPosition = new Vector2 (0, i);
+
+            gObj.GetComponentInChildren<Text> ().text = 
+                questDatas [i].Idx + ". " + questDatas [i].Name;
+
+            gObj.SetActive (true);
+        }
+    }
+}
